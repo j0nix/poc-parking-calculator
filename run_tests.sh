f534758bb720a6e69c1f2f4cd1bcd11a946c17e9
Jon Svendsen <josv01@j0nixRulez.home>
2017-10-03 09:43:23 +0200

    fixing stuff with tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,10 @@
-phpunit --testdox-html tests.html
-phpunit --testdox-text tests.txt
+#!/bin/bash
+
+if [[ $1 == "html" ]]
+then
+	phpunit --testdox-html tests.html
+else
+	phpunit --testdox-text tests.txt 
+	printf "\n\n---- TEST RESULTS ----\n\n"
+	cat tests.txt
+fi
